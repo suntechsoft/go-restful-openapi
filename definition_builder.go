b14a23d95f7c7efe70253498a0e95fcff1439552
Dmytrii Andriichuk <d.andriichuk@globalgames.net>
2020-10-07 12:22:24 +0300

    fix(definition): string type of json marshaler

--- definition_builder.go
+++ definition_builder.go
@@ -161,19 +161,6 @@ func (b definitionBuilder) buildProperty(field reflect.StructField, model *spec.
 	}
 	fieldType := field.Type
 
-	// check if type is doing its own marshalling
-	marshalerType := reflect.TypeOf((*json.Marshaler)(nil)).Elem()
-	if fieldType.Implements(marshalerType) {
-		var pType = "string"
-		if prop.Type == nil {
-			prop.Type = []string{pType}
-		}
-		if prop.Format == "" {
-			prop.Format = b.jsonSchemaFormat(keyFrom(fieldType, b.Config))
-		}
-		return jsonName, modelDescription, prop
-	}
-
 	// check if annotation says it is a string
 	if jsonTag := field.Tag.Get("json"); jsonTag != "" {
 		s := strings.Split(jsonTag, ",")
